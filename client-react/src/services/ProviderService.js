3e47febeb3aba1abd6188c7697bf85740970dfe0
armando maikol duran salas <maikold774@gmail.com>
2025-11-02 09:33:16 -0500

    feat: the API Key submission in providerService was created

--- client-react/src/services/ProviderService.js
+++ client-react/src/services/ProviderService.js
@@ -1,14 +1,23 @@
 import axios from 'axios';
 
+// Load API key from environment variables.
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 // Base URL of the API.
 const API_BASE_URL = 'http://localhost:5262/api/Providers';
 
+const config = {
+    headers: {
+        'X-API-Key': API_KEY 
+    }
+};
 
  // Executes the GET request to retrieve providers with filtering/paging.
 
 export const getProviders = async (params) => {
     try {
-        const response = await axios.get(API_BASE_URL, { params });
+        const response = await axios.get(API_BASE_URL, { params, ...config });
         return response.data.$values || []; 
     } catch (error) {
         console.error("API Call Error in getProviders:", error);
@@ -21,7 +30,7 @@ export const getProviders = async (params) => {
  
 export const createProvider = async (providerData) => {
     try {
-        const response = await axios.post(API_BASE_URL, providerData);
+        const response = await axios.post(API_BASE_URL, providerData, config);
         return response.data; 
     } catch (error) {
         console.error("API Call Error in createProvider:", error);
